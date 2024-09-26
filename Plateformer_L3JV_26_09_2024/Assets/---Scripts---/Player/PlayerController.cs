b0cc18245f758858534ecc03852870c6bcd877f9
P3D\hugo.lestriez <lestriez.hugo62@gmail.com>
2024-09-26 18:17:03 +0200

    Commencer les jumps et leur conditions

--- Plateformer_L3JV_26_09_2024/Assets/---Scripts---/Player/PlayerController.cs
+++ Plateformer_L3JV_26_09_2024/Assets/---Scripts---/Player/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using UnityEditor.Rendering;
 using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -20,18 +21,21 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _gravityMultiplier;
 
     private float _gravity = -9.81f;
+    private float _currentTimerBetweenJumps;
+    private int _currentNumbOfJumps;
     private Vector2 _inputs;
     private Vector2 _direction = Vector2.zero;
-    private bool _inputJump;
+    private bool _canJump;
     private bool _isGrounded;
 
     private void Start()
     {
         TeleportPlayerToSpawnPoint();
+        _currentTimerBetweenJumps = 0;
     }
     private void Update()
     {
-        Debug.Log(_inputs);
+        _currentTimerBetweenJumps += Time.deltaTime;
     }
     private void FixedUpdate()
     {
@@ -45,16 +49,35 @@ public class PlayerController : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         _inputs = context.ReadValue<Vector2>();
-        //_direction = new Vector3(_inputs.x, _inputs.y);
     }
     public void Jump(InputAction.CallbackContext context)
     {
         if (!context.performed) return;
+        CheckJumpConditions();
+        if (_canJump)
+        {
+        }
         _rigidbody.AddForce(new Vector2(0, _jumpForce), ForceMode2D.Impulse);
     }
     private void ApplyMovements()
     {
         _rigidbody.AddForce(new Vector2(0, 1) * _gravity * _gravityMultiplier, ForceMode2D.Force);
         _rigidbody.AddForce(_inputs * _speed, ForceMode2D.Force);
+    }
+    //private void CheckIsGrounded()
+    //{
+    //    _currentNumbOfJumps = 0;
+    //}
+    private void CheckJumpConditions()
+    {
+        if (_minTimerBetweenJumps <= _currentTimerBetweenJumps && _numberOfJumps > _currentTimerBetweenJumps)
+        {
+            _currentTimerBetweenJumps = 0;
+            _canJump = true;
+        }
+        else
+        {
+            _canJump = false;
+        }
     }
 }
