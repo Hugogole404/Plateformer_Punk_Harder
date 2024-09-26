12a447be1f69b8322a4cf559a8cd11b137fe930f
1Coquo <1coquo@gmail.com>
2024-09-26 17:33:41 +0200

    PlatformMovement

--- Plateformer_L3JV_26_09_2024/Assets/---Scripts---/PlatformMovement.cs
+++ Plateformer_L3JV_26_09_2024/Assets/---Scripts---/PlatformMovement.cs
@@ -2,21 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bumpers : MonoBehaviour
+public class PlatformMovement : MonoBehaviour
 {
-    float _distance;
-    float _time;
-    [SerializeField] bool sens;
+    [SerializeField] float _distance;
+    [SerializeField] float _time;
+    [SerializeField] bool _sens;
+    Vector2 _position;
+    float _offset = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _position = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //transform.x = 
+        _offset += Time.deltaTime;
+        print(Mathf.Sin(_offset));
+        transform.position = new Vector2(Mathf.Sin(_offset * _distance * 4), 0);
     }
 }
